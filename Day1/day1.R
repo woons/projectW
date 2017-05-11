16d608068a1da81ea2f34ad3dbf31d856fceb456
Yeowoon <woons.2016@gmail.com>
2017-05-11 21:44:01 +0900

    day1 added

--- Day1/day1.R
+++ Day1/day1.R
@@ -1,1 +1,55 @@
-dd
+## 다시부산 R코드 작업
+library(tidyverse)
+library(stringr)
+library(rvest)
+library(httr)
+
+EmptyVectorMain <- NULL
+
+#total 687pages
+for(i in 1:4){
+  url <- paste0("http://biff.kr/kor/html/archive/arc_history_tsearch.asp?page=",i,"&piff_code=&s1=&search=&keyword=")
+  res <- GET(url)
+  h <- read_html(res)
+  
+  #작품이름
+  title <- html_nodes(h, "div.txt > h5") %>% 
+    html_text
+  title2 <- str_split_fixed(title, " / ", 2) %>% as.data.frame()
+  colnames(title2) <- c("title_kr", "title_en")
+  
+  #본문텍스트
+  content <- html_nodes(h, "p.note") %>% 
+    html_text %>% 
+    str_trim
+  
+  #러닝타임
+  time <- html_nodes(h, "span.program_Op1") %>% 
+    html_text %>% 
+    str_sub(7, 13) %>% 
+    str_trim
+  
+  #감독, 출품국가, 출품년도 for second loop
+  director <- NULL
+  country <- NULL
+  year <- NULL
+  for(j in 1:8){
+    
+    #director
+    path = paste0("//*[@id='content_text']/div/ul/li[", j, "]/div[2]/p[1]/text()[1]")
+    directorInside <- html_nodes(h, xpath = path) %>% html_text
+    director <- c(director, directorInside) %>% str_replace("감독 : ", "")
+    
+    #year
+    path2 = paste0("//*[@id='content_text']/div/ul/li[", j, "]/div[2]/p[1]/text()[2]")
+    yearInside <- html_nodes(h, xpath = path2) %>% html_text
+    year <- c(year, yearInside) %>% str_replace("출품년도 : ", "") %>% str_sub(1, 4)
+    
+    #country
+    path3 = paste0("//*[@id='content_text']/div/ul/li[", j, "]/div[2]/p[1]/text()[3]")
+    countryInside <- html_nodes(h, xpath = path3) %>% html_text
+    country <- c(country, countryInside) %>% str_replace("국가 : ", "")
+  } 
+  EmptyVectorInside <- data.frame(title2, content, time, director, country, year, stringsAsFactors = F)
+  EmptyVectorMain <- rbind(EmptyVectorMain, EmptyVectorInside)
+}
---
+++ Day1/day1_description.md
@@ -0,0 +1,33 @@
+Day1 Exercise
+================
+
+#### Biff 출연작들 크롤링
+
+CSS Selector로 쉽게 크롤링하려고 했지만 웹페이지의 비효율적 구조로 문제가 있음
+loop within loop를 활용해야만 했던 과정의 코드
+
+Including Code
+--------------
+
+You can include R code in the document as follows:
+
+``` r
+summary(cars)
+```
+
+    ##      speed           dist       
+    ##  Min.   : 4.0   Min.   :  2.00  
+    ##  1st Qu.:12.0   1st Qu.: 26.00  
+    ##  Median :15.0   Median : 36.00  
+    ##  Mean   :15.4   Mean   : 42.98  
+    ##  3rd Qu.:19.0   3rd Qu.: 56.00  
+    ##  Max.   :25.0   Max.   :120.00
+
+Including Plots
+---------------
+
+You can also embed plots, for example:
+
+![](day1_description_files/figure-markdown_github/pressure-1.png)
+
+Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
